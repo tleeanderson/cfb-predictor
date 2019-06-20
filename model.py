07dddecfc6a53afefeb5dd0d550beb5480fd3d2a
Trever Anderson <treveranderson01@gmail.com>
2019-06-19 20:16:58 -0500

    split out defensive coded

--- model.py
+++ model.py
@@ -15,7 +15,8 @@ def read_file(**kwargs):
         for row in csv_reader:
             game_code_key, team_code_key = "Game Code", "Team Code"
             game_code_id, team_code_id = row[game_code_key], row[team_code_key]
-            team_game_stats = {k: row[k] for k in set(row.keys()).difference({game_code_key, team_code_key})}
+            team_game_stats = {k: row[k] for k in set(row.keys())
+                               .difference({game_code_key, team_code_key})}
             if game_code_id in result:
                 result[game_code_id][team_code_id] = team_game_stats
             else:
@@ -23,7 +24,9 @@ def read_file(**kwargs):
                 result[game_code_id][team_code_id] = team_game_stats
     return result
 
-def check_team_stats():
+def check_team_stats(**kwargs):
+    team_stats = kwargs['team_stats']
+
     correct, incorrect = [], []
     for k, v in team_stats.iteritems():
         if len(v.keys()) == 2:
@@ -36,6 +39,7 @@ def check_team_stats():
 
 def main(unused_argv):
     team_stats = read_file(input_file=DATA_PATH)
+    check_team_stats(team_stats=team_stats)
 
 if __name__ == '__main__':
     logging.getLogger("tensorflow").setLevel(logging.INFO)
