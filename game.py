973b1d542009e99c7db2dfe95ce8dcb39029aed2
Trever Anderson <treveranderson01@gmail.com>
2019-06-20 20:43:22 -0500

    can get team's games by game code

--- game.py
+++ game.py
@@ -9,7 +9,29 @@ def csv_to_map(**kwargs):
     result = {}
 
     for row in csv_reader:
-        game_code_id = "Game Code"
+        game_code_id = 'Game Code'
         result[row[game_code_id]] = {k: row[k] for k in set(row.keys())
                            .difference({game_code_id})}
     return result
+
+def team_games_by_game_code(**kwargs):
+    game_code_id, games = kwargs['game_code_id'], kwargs['games']
+    visit_team, home_team = 'Visit Team Code', 'Home Team Code'
+
+    games_by_team = {}
+    games_by_team[games[game_code_id][visit_team]] = []
+    games_by_team[games[game_code_id][home_team]] = []
+
+    for gid, game_info in games.iteritems():
+        if game_info[visit_team] in games_by_team:
+            games_by_team[game_info[visit_team]].append({gid: game_info})
+        if game_info[home_team] in games_by_team:
+            games_by_team[game_info[home_team]].append({gid: game_info})
+
+    return games_by_team
+        
+        
+            
+        
+
+    
--- model.py
+++ model.py
@@ -12,21 +12,36 @@ def loop_through(**kwargs):
     for k, v in data.iteritems():
         print("k " + str(k))
         print("v " + str(v))
+        print("len(v) " + str(len(v)))
         raw_input() 
+
+def team_game_stats(**kwargs):
+    input_directory = kwargs['directory']
+
+    tgs_file = path.join(input_directory, tgs.FILE_NAME)
+    team_game_stats = util.read_file(input_file=tgs_file, func=tgs.csv_to_map)
+    converted_tgs = tgs.alter_types(type_mapper=tgs.type_mapper, 
+                                    game_map=team_game_stats)
+    labels = tgs.add_labels(team_game_stats=converted_tgs)
+    
+    return labels
+
+def game_stats(**kwargs):
+    input_directory = kwargs['directory']
+
+    game_file = path.join(input_directory, game.FILE_NAME)
+    game_data = util.read_file(input_file=game_file, func=game.csv_to_map)
+    
+    return game_data
 
 def main(args):
     if len(args) == 2:
         input_directory = args[1]
-        tgs_file = path.join(input_directory, tgs.FILE_NAME)
-        game_file = path.join(input_directory, game.FILE_NAME)
+        stats = team_game_stats(directory=input_directory)
+        game_data = game_stats(directory=input_directory)
+        games_by_team = game.team_games_by_game_code(games=game_data, game_code_id='0365002820050910')
 
-        team_game_stats = util.read_file(input_file=tgs_file, func=tgs.csv_to_map)
-        converted_tgs = tgs.alter_types(type_mapper=tgs.type_mapper, 
-                                        game_map=team_game_stats)
-        labels = tgs.add_labels(team_game_stats=converted_tgs)
-
-        game_data = util.read_file(input_file=game_file, func=game.csv_to_map)
-        loop_through(data=game_data)
+        loop_through(data=games_by_team)
 
 if __name__ == '__main__':
     logging.getLogger("tensorflow").setLevel(logging.INFO)
