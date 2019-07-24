a00548f430415a09392ff3d481b64f847022c7f8
Trever Anderson <treveranderson01@gmail.com>
2019-07-23 20:37:21 -0500

    wrote tests for utilities and documented

---
+++ test/test_utilities.py
@@ -0,0 +1,64 @@
+import utilities as util
+import operator as op
+
+def test_read_file():
+    in_file = 'file_that_doesnt_exist'
+
+    def file_reader(**kwargs):
+        csv = kwargs['csv_reader']
+        return None
+
+    try:
+        out = util.read_file(input_file=in_file, func=file_reader)
+    except IOError:
+        assert(True)
+    else:
+        assert(False)
+
+def test_convert_type():
+    types, bad_val, good_val = (int, float), 'f', '1'
+
+    out = util.convert_type(types=types, value=good_val)
+
+    assert(out == 1)
+    try:
+        util.convert_type(types=types, value=bad_val)
+    except ValueError:
+        assert(True)
+    else:
+        assert(False)
+
+def test_subset_of_map():
+    in_data, take_out = {i: i for i in range(10)}, {i for i in range(5)}
+
+    out = util.subset_of_map(full_map=in_data, take_out_keys=take_out)
+
+    assert(set(in_data.keys()).difference(take_out) == set(out.keys()))
+
+def test_merge_maps():
+    m1 = {i: i for i in range(1, 2)}
+    m2 = {i: i for i in m1.keys()}
+
+    out_add = util.merge_maps(map1=m1, map2=m2, merge_op=op.add)
+    out_sub = util.merge_maps(map1=m1, map2=m2, merge_op=op.sub)
+    m3 = {i: i for i in set(m1.keys()).union({45})}
+    
+    assert(list(out_add.iteritems())[0][1] == 2)
+    assert(list(out_sub.iteritems())[0][1] == 0)
+    try:
+        util.merge_maps(map1=m1, map2=m3, merge_op=op.add)
+    except ValueError:
+        assert(True)
+    else:
+        assert(False)
+
+def test_create_map():
+    nv = 3
+    keys, default = {i for i in range(nv)}, 'val'
+
+    out = util.create_map(keys=keys, default=default)
+    assert(len(out) == nv)
+    assert(map(lambda v: v == default, out.values()))
+    
+
+    
--- utilities.py
+++ utilities.py
@@ -3,6 +3,15 @@ import csv
 #functions that serve a general purpose
 
 def read_file(**kwargs):
+    """Passes a list of values to a given function, saves the result 
+       and returns it
+
+    Args:
+         input_file: csv file with data
+         func: function to map the list of values to something else
+    
+    Returns: result of func accepting list of values
+    """
     input_file, func = kwargs['input_file'], kwargs['func']
     result = None
 
@@ -12,6 +21,20 @@ def read_file(**kwargs):
     return result
 
 def convert_type(**kwargs):
+    """Converts a value to a given type, returns value error 
+       if unsuccessful
+
+    Args:
+        types: collection of types, first type to succeed 
+               will be resulting type
+        value: a value
+    
+    Returns: value
+    
+    Raises: 
+           ValueError: if value cannot be casted to one of
+                       the given types
+    """
     types, value = kwargs['types'], kwargs['value']
 
     for t in types:
@@ -22,11 +45,32 @@ def convert_type(**kwargs):
     raise ValueError("Could not convert %s" % (value))
 
 def subset_of_map(**kwargs):
+    """Given a map and a set of keys, returns a map with the 
+       specified keys removed
+
+    Args:
+         full_map: a map
+         take_out_keys: keys to remove from map
+    
+    Returns: a map
+    """
     full_map, take_out_keys = kwargs['full_map'], kwargs['take_out_keys']
 
     return {k: full_map[k] for k in set(full_map.keys()).difference(take_out_keys)}
 
 def merge_maps(**kwargs):
+    """Given two maps, merges them with the specified merge_op
+
+    Args:
+         map1: a map
+         map2: a map
+         merge_op: an operator to merge map1 and map2
+
+    Returns: a map
+
+    Raises: 
+           ValueError: if map1.keys() != map2.keys()
+    """
     map1, map2, merge_op = kwargs['map1'], kwargs['map2'], kwargs['merge_op']
     
     if set(map1.keys()) == set(map2.keys()):
@@ -36,6 +80,14 @@ def merge_maps(**kwargs):
                          (str(set(map1.keys()).difference(map2.keys()))))
 
 def create_map(**kwargs):
+    """Creates a map with default values given keys and a value
+
+    Args:
+         keys: set of keys for the map
+         default: value for all keys
+    
+    Returns: a map
+    """
     keys, default = kwargs['keys'], kwargs['default']
 
     return {k: default for k in keys}
